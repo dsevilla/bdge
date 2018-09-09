4fa19fb339e3a5461277374e8ab8879cca0873c4
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2018-09-09 21:12:10 +0200

    Review intro.

--- hbase/start-jupyter.sh
+++ hbase/start-jupyter.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-docker run --rm -it --user root -e 'GRANT_SUDO=yes' -e "NB_UID=$(id -u)" -p 8888:8888 --link master.krejcmat.com:hbase -v `pwd`/..:/home/jovyan/bdge jupyter/all-spark-notebook start-notebook.sh --NotebookApp.token=''
+docker run --rm -it --user root -e 'GRANT_SUDO=yes' -e "NB_UID=$(id -u)" -p 8888:8888 --link master.krejcmat.com:hbase -v `pwd`/..:/home/jovyan/bdge jupyter/scipy-notebook start-notebook.sh --NotebookApp.token=''
