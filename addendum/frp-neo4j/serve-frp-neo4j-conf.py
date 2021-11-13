01e08cfa983ed34e998fc91914b285e14f636692
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2021-11-13 22:09:47 +0100

    Server corrections

--- addendum/frp-neo4j/serve-frp-neo4j-conf.py
+++ addendum/frp-neo4j/serve-frp-neo4j-conf.py
@@ -2,23 +2,21 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 
-hostName = "localhost"#"neuromancer.inf.um.es"
+hostName = "neuromancer.inf.um.es"
 serverPort = 8080
 
-class MyServer(BaseHTTPRequestHandler):
-  
-    # modulo configurations
-    mod_cn = 30
+# modulo configurations
+mod_cn = 30
 
-    # Base port
-    base_port = 8082
+# Base port
+base_port = 8082
 
-    frpc_conf = '''
+frpc_conf = '''
 [common]
 server_addr = 155.54.204.57
 server_port = 8081
 
-[ssh{0}]
+[browser{0}]
 type = tcp
 local_ip = 127.0.0.1
 local_port = 7474
@@ -31,19 +29,21 @@ local_port = 7687
 remote_port = {1}
 '''
 
-    # conn number, modulo mod_cn.
-    # Ports numbers are: base_port + (cn * 2), base_port + (cn*2) + 1 
-    cn = 0
+# conn number, modulo mod_cn.
+# Ports numbers are: base_port + (cn * 2), base_port + (cn*2) + 1 
+cn = 0
 
+class MyServer(BaseHTTPRequestHandler):
+  
     def do_GET(self):
+        global frpc_conf, cn, mod_cn, base_port
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes(self.frpc_conf.format(self.base_port + (self.cn*2),
-            self.base_port + 1 + (self.cn*2)),
-            "utf-8"))
-        self.cn = self.cn + 1
-        self.cn = self.cn % self.mod_cn
+        self.wfile.write(bytes(frpc_conf.format(base_port + (cn*2),
+            base_port + 1 + (cn*2)), "utf-8"))
+        cn = cn + 1
+        cn = cn % mod_cn
 
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), MyServer)
