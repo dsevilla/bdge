18a31feaf75667635d4c7cab4255fc3bfc1e4827
Diego Sevilla <dsevilla@um.es>
2025-08-03 01:04:55 +0200

    bdge_csv_utils.

--- addendum/bdge_csv_utils/setup.py
+++ addendum/bdge_csv_utils/setup.py
@@ -3,7 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name='bdge_csv_utils',
     version='0.1.0',
-    packages=find_packages(),
+    packages=find_packages(include=[
+        'bdge_csv_utils',
+        'bdge_csv_utils.csv_schema',
+        'bdge_csv_utils.csv_schema.*',
+        'bdge_csv_utils.mongo_utils',
+        'bdge_csv_utils.mongo_utils.*',
+    ]),
     install_requires=[],
     url='https://github.com/dsevilla/bdge/addendum/bdge_csv_utils',
     author='Your Name',
