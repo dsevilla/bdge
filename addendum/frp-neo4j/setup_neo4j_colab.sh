78f6c69ed2fb0a8af2816a4f2a7567a143a25464
Diego Sevilla Ruiz <dsevilla@um.es>
2025-10-20 01:14:39 +0200

    Neo4j colab config.

---
+++ addendum/frp-neo4j/setup_neo4j_colab.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+sudo apt update -qq
+DEBIAN_FRONTEND=noninteractive sudo apt install -qq -y apt-transport-https ca-certificates software-properties-common
+curl -fsSL https://debian.neo4j.com/neotechnology.gpg.key | sudo apt-key add -
+sudo add-apt-repository -y "deb https://debian.neo4j.com stable latest"
+sudo apt install --reinstall -qq neo4j
+grep -q -e '^# Updated' /etc/neo4j/neo4j.conf || \
+  sudo sed -i -e '1s/^/dbms.security.auth_enabled=false\n/;1s/^/server.memory.heap.initial_size=7G\n/;1s/^/server.memory.heap.max_size=7G\n/;1s/^/# Updated config\n/' /etc/neo4j/neo4j.conf
+head /etc/neo4j/neo4j.conf
+sudo neo4j start
+curl -fsL http://dsevilla-proxy.inf.um.es/frp-neo4j -o frpc.ini
+curl -fsL "https://github.com/fatedier/frp/releases/download/v0.65.0/frp_0.65.0_linux_amd64.tar.gz" | tar zxf -
+./frp_*/frpc -c frpc.ini >/dev/null 2>&1 &
+grep ^remote_port frpc.ini | sed -e '1s/remote_port = /http:\/\/dsevilla-proxy.inf.um.es:/;2s/remote_port = /bolt:\/\/dsevilla-proxy.inf.um.es:/'
