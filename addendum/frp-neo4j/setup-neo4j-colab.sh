f8c94e405a28a5aaad879b3bcbd39a33a12f2e27
diego sevilla <dsevilla@ditec.um.es>
2025-11-20 12:28:23 +0100

    x

--- addendum/frp-neo4j/setup-neo4j-colab.sh
+++ addendum/frp-neo4j/setup-neo4j-colab.sh
@@ -4,7 +4,7 @@ sudo apt update -qq
 DEBIAN_FRONTEND=noninteractive sudo apt install -qq -y apt-transport-https ca-certificates software-properties-common
 curl -fsSL https://debian.neo4j.com/neotechnology.gpg.key | sudo apt-key add -
 sudo add-apt-repository -y "deb https://debian.neo4j.com stable latest"
-DEBIAN_FRONTEND=noninteractive sudo apt install --reinstall -qq neo4j
+DEBIAN_FRONTEND=noninteractive sudo apt install --reinstall -y -qq neo4j
 grep -q -e '^# Updated' /etc/neo4j/neo4j.conf || \
   sudo sed -i -e '1s/^/dbms.security.auth_enabled=false\n/;1s/^/server.memory.heap.initial_size=7G\n/;1s/^/server.memory.heap.max_size=7G\n/;1s/^/# Updated config\n/' /etc/neo4j/neo4j.conf
 head /etc/neo4j/neo4j.conf
