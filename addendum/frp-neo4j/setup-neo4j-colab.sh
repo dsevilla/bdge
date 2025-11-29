7d1f557b862700869718620c20358280214b1ca4
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2025-11-29 15:28:11 +0100

    6g en vez de 7g para neo4j.

--- addendum/frp-neo4j/setup-neo4j-colab.sh
+++ addendum/frp-neo4j/setup-neo4j-colab.sh
@@ -6,7 +6,7 @@ curl -fsSL https://debian.neo4j.com/neotechnology.gpg.key | sudo apt-key add -
 sudo add-apt-repository -y "deb https://debian.neo4j.com stable latest"
 DEBIAN_FRONTEND=noninteractive sudo apt install --reinstall -y -qq neo4j
 grep -q -e '^# Updated' /etc/neo4j/neo4j.conf || \
-  sudo sed -i -e '1s/^/dbms.security.auth_enabled=false\n/;1s/^/server.memory.heap.initial_size=7G\n/;1s/^/server.memory.heap.max_size=7G\n/;1s/^/# Updated config\n/' /etc/neo4j/neo4j.conf
+  sudo sed -i -e '1s/^/dbms.security.auth_enabled=false\n/;1s/^/server.memory.heap.initial_size=6G\n/;1s/^/server.memory.heap.max_size=6G\n/;1s/^/# Updated config\n/' /etc/neo4j/neo4j.conf
 head /etc/neo4j/neo4j.conf
 sudo neo4j start
 curl --connect-timeout 10 --max-time 30 --retry 5 --retry-delay 2 -fsL http://155.54.204.149/frp-neo4j -o frpc.ini || exit 1
