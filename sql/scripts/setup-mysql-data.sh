2b30a519640da48c21a62494296dc74e1b330f80
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2025-12-01 08:41:50 +0100

    Cambio de stackoverflow.sql.gz a es.stackoverflow.sql.gz.

--- sql/scripts/setup-mysql-data.sh
+++ sql/scripts/setup-mysql-data.sh
@@ -11,13 +11,13 @@ BASE_DIR="${1:-.}"
 test -e "${BASE_DIR}/sql" && BASE_DIR="${BASE_DIR}/sql"
 
 # Already exists? exit
-test -e "${BASE_DIR}/data/stackoverflow.sql.gz" && test -e "${BASE_DIR}/data/done" && exit 0
+test -e "${BASE_DIR}/data/es.stackoverflow.sql.gz" && test -e "${BASE_DIR}/data/done" && exit 0
 
 BASE_URL="https://github.com/dsevilla/bd2-data/raw/refs/heads/main/es.stackoverflow/mysql"
 
-for f in `wget -qO - ${BASE_URL}/stackoverflow.sql.gz.list.txt`; do
+for f in `wget -qO - ${BASE_URL}/es.stackoverflow.sql.gz.list.txt`; do
     wget -qO - "${BASE_URL}/$f"
-done > "${BASE_DIR}/data/stackoverflow.sql.gz"
+done > "${BASE_DIR}/data/es.stackoverflow.sql.gz"
 
 # Signal we're done
 touch "${BASE_DIR}/data/done"
